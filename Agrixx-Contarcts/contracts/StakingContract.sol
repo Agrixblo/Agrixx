bd08ef67d37e14e6cc7c48e438188332d19b7f0a
Amaan <amaansayyad@yahoo.com>
2024-05-31 16:18:04 +0530

    Update StakingContract.sol

--- Agrixx-Contarcts/contracts/StakingContract.sol
+++ Agrixx-Contarcts/contracts/StakingContract.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "./MyToken.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract StakingContract {
+contract StakingContract is VRFConsumerBase {
     using SafeMath for uint256;
 
     AGIXXToken public agixxToken;
@@ -12,8 +13,16 @@ contract StakingContract {
     mapping(address => uint256) public rewards;
     uint256 public totalStaked;
 
-    constructor(address _agixxToken) {
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+
+    constructor(address _agixxToken, address _vrfCoordinator, address _linkToken, bytes32 _keyHash, uint256 _fee)
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+    {
         agixxToken = AGIXXToken(_agixxToken);
+        keyHash = _keyHash;
+        fee = _fee;
     }
 
     function stake(uint256 amount) external {
@@ -40,7 +49,29 @@ contract StakingContract {
     }
 
     function updateReward(address user) internal {
-        uint256 performanceFactor = 1; // Assume 1 for now
+        uint256 performanceFactor = getPerformanceFactor();
         rewards[user] = rewards[user].add(stakes[user].mul(performanceFactor).div(100)); // 1% of stake as reward
+    }
+
+    function getPerformanceFactor() internal view returns (uint256) {
+        // Here we use the randomResult as the performance factor for simplicity.
+        // In a real-world scenario, this could be more complex.
+        return randomResult % 10 + 1; // Just as an example, returning a value between 1 and 10
+    }
+
+    function requestRandomnessForReward() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK to pay fee");
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+    }
+
+    // function to withdraw LINK tokens from the contract
+    function withdrawLink() external onlyOwner {
+        uint256 balance = LINK.balanceOf(address(this));
+        require(balance > 0, "No LINK to withdraw");
+        LINK.transfer(msg.sender, balance);
     }
 }
